096edba4aa0d533173d8dc56caaad434d14637f1
Priyanka Bawanna <priyankabawanna@Priyankas-MacBook-Air.local>
2025-02-18 23:01:15 +0530

    Form

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import './App.css';
 import Table from './Table';
 import Model from './Model';
 import Form from './Form';
-import XModal from './XModal';
 
 function App() {
   return (
     <div className="d-flex justify-content-center flex-column align-items-center">
-     <XModal />
+      <h1>User Details Modal</h1>
+      <Model>
+      <Form/>
+      </Model>
+
   
     </div>
   );
