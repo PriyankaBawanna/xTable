f7b6379e5cf0ebfcf4130c8fc866b7e2d6fa9386
Priyanka Bawanna <priyankabawanna@Priyankas-MacBook-Air.local>
2025-02-23 13:17:41 +0530

    CSS class update

--- src/Model.js
+++ src/Model.js
@@ -16,14 +16,14 @@ const Model = ({ children }) => {
   };
 
   return (
-    <div className="d-flex">
+    <div className="d-flex model">
       <button onClick={handleShowModel} type="button" className="btn btn-primary">
         Open Form
       </button>
 
       {show && (
         <div
-          className="modal-overlay w-100 h-100 d-flex justify-content-center align-items-center position-fixed top-0 start-0 bg-black bg-opacity-75"
+          className="modal-overlay w-100 h-100 d-flex justify-content-center align-items-center position-fixed top-0 start-0 bg-black bg-opacity-75 modal-content"
           onClick={handleCloseModal}
         >
           <div className="modal-content bg-white w-50 p-4 rounded" onClick={(e) => e.stopPropagation()}>
